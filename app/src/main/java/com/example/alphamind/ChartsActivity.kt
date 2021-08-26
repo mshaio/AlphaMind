9f31fdaa05d321a74ba2b6d7951493f0fb4924eb
mark <mark@used.ca>
2021-08-26 15:28:26 -0700

    Added today's best and personal best

--- app/src/main/java/com/example/alphamind/ChartsActivity.kt
+++ app/src/main/java/com/example/alphamind/ChartsActivity.kt
@@ -27,6 +27,11 @@ import com.google.android.material.progressindicator.CircularProgressIndicator
 import com.google.android.material.chip.Chip
 import io.realm.Realm
 import io.realm.RealmResults
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.*
+import kotlin.collections.ArrayList
 
 //import kotlin.math
 
@@ -64,6 +69,20 @@ class ChartsActivity : AppCompatActivity() {
 
         val dayExerciseVolume: TextView = findViewById(R.id.day_stats_volume)
 
+        val tv_today_best_one: TextView = findViewById(R.id.tv_today_best_one)
+        val tv_today_best_two: TextView = findViewById(R.id.tv_today_best_two)
+        val tv_today_best_three: TextView = findViewById(R.id.tv_today_best_three)
+        val tv_today_best_four: TextView = findViewById(R.id.tv_today_best_four)
+        val tv_today_best_five: TextView = findViewById(R.id.tv_today_best_five)
+        var tv_group_todays_best = arrayListOf<TextView>()
+        tv_group_todays_best.add(tv_today_best_one)
+        tv_group_todays_best.add(tv_today_best_two)
+        tv_group_todays_best.add(tv_today_best_three)
+        tv_group_todays_best.add(tv_today_best_four)
+        tv_group_todays_best.add(tv_today_best_five)
+
+        val tv_personal_best: TextView = findViewById(R.id.tv_personal_best)
+
         dayExerciseVolume.text = totalExerciseVolume
 //        dayExerciseVolume.setTextColor(Color.parseColor("#A92A04"))
         dayExerciseVolume.setTypeface(null,Typeface.BOLD)
@@ -72,12 +91,6 @@ class ChartsActivity : AppCompatActivity() {
         chart_three.progress = 0
         chart_four.progress = 0
         chart_five.progress = 0
-
-//        chip_one.setChipBackgroundColorResource(R.color.black)
-//        chip_two.setChipBackgroundColorResource(R.color.black)
-//        chip_three.setChipBackgroundColorResource(R.color.black)
-//        chip_four.setChipBackgroundColorResource(R.color.black)
-//        chip_five.setChipBackgroundColorResource(R.color.black)
 
         chip_one.isInvisible = true
         chip_two.isInvisible = true
@@ -90,8 +103,10 @@ class ChartsActivity : AppCompatActivity() {
         Realm.init(this)
         val realm = Realm.getDefaultInstance()
         val activities = realm.where(ExerciseModel::class.java).equalTo("date", exerciseDate).findAll()
+        lateinit var exerciseType: String
 
         for (activity in activities) {
+            exerciseType = activity.exerciseType
             if (!exerciseVolumeMap.containsKey(activity.activity)) {
                 exerciseVolumeMap[activity.activity] = activity.weights * activity.reps
             } else {
@@ -168,7 +183,24 @@ class ChartsActivity : AppCompatActivity() {
             chip_five.setChipBackgroundColorResource(R.color.teal_700)
         }
 
+        println(getTodaysBests(exerciseDate))
+        var todaysBests: MutableMap<String,Int> = getTodaysBests(exerciseDate)
+        var personalBests: MutableMap<String,Int> = getPersonalBests(exerciseType)
+        val numberOfExercises = todaysBests.size
 
+//        for (i in 0..numberOfExercises) {
+//            tv_group_todays_best[i].text =
+//        }
+
+        for ((exerciseItem, heaviestWeight) in todaysBests) {
+            tv_today_best_one.text = tv_today_best_one.text.toString() + exerciseItem + ": " + heaviestWeight.toString() + " lbs" + "\n"
+        }
+
+        for ((exerciseItem, heaviestWeight) in personalBests) {
+            tv_personal_best.text = tv_personal_best.text.toString() + exerciseItem + ": " + heaviestWeight.toString() + " lbs" + "\n"
+        }
+
+        println(getPastDate(7))
 
         val entries: ArrayList<BarEntry> = ArrayList()
         entries.add(BarEntry(1f, 4f))
@@ -209,10 +241,6 @@ class ChartsActivity : AppCompatActivity() {
         //draw chart
         barChart.invalidate()
 
-
-
-
-
 //        binding = ActivityChartsBinding.inflate(layoutInflater)
 //        setContentView(binding.root)
 //
@@ -226,6 +254,63 @@ class ChartsActivity : AppCompatActivity() {
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                .setAction("Action", null).show()
 //        }
+    }
+
+    private fun computeBests(activities: RealmResults<ExerciseModel>, exerciseWeightMap: MutableMap<String,Int>): MutableMap<String,Int> {
+        for (activity in activities) {
+            if (!exerciseWeightMap.containsKey(activity.activity)) {
+                exerciseWeightMap[activity.activity] = activity.weights
+            } else {
+                print("!@!@")
+                if (exerciseWeightMap[activity.activity]!! < activity.weights)
+                    exerciseWeightMap[activity.activity] = activity.weights
+            }
+        }
+        return exerciseWeightMap
+    }
+
+    private fun initialiseRealmDB(): Realm {
+        Realm.init(this)
+        return Realm.getDefaultInstance()
+    }
+
+    private fun getTodaysBests(date:String): MutableMap<String,Int> {
+        Realm.init(this)
+        val realm = Realm.getDefaultInstance()
+        val activities = realm.where(ExerciseModel::class.java).equalTo("date", date).findAll()
+        var exerciseWeightMap = mutableMapOf<String,Int>()
+        return computeBests(activities,exerciseWeightMap)
+    }
+
+    private fun getPersonalBests(exerciseType: String):MutableMap<String,Int> {
+        Realm.init(this)
+        val realm = Realm.getDefaultInstance()
+        val activities = realm.where(ExerciseModel::class.java).equalTo("exerciseType", exerciseType).findAll()
+        var exerciseWeightMap = mutableMapOf<String,Int>()
+        return computeBests(activities,exerciseWeightMap)
+    }
+
+    private fun getPastDate(daysAgo: Int): Date {
+        val calendar = Calendar.getInstance()
+        calendar.add(Calendar.DATE, -daysAgo)
+        return calendar.time
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getLastSameSevenExercises(exerciseType: String): MutableMap<String,Int> {
+//        val currentDateTime = LocalDateTime.now()
+//        val todaysDate = currentDateTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT))
+        val realm: Realm = initialiseRealmDB()
+        val samePastSevenExercises = realm.where(ExerciseModel::class.java).equalTo("exerciseType", exerciseType).findAll()
+        lateinit var weightMapByDate: MutableMap<String,Int>
+        for (pastExercise in samePastSevenExercises) {
+            if (!weightMapByDate.containsKey(pastExercise.date)) {
+                weightMapByDate[pastExercise.date.toString()] = pastExercise.weights * pastExercise.reps
+            } else {
+                weightMapByDate[pastExercise.date.toString()] = weightMapByDate[pastExercise.date.toString()]!! + pastExercise.weights * pastExercise.reps
+            }
+        }
+        return weightMapByDate
     }
 
     override fun onSupportNavigateUp(): Boolean {
